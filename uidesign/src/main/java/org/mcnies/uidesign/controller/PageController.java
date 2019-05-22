9178d5276c5db3ba2ebd113960773cb668d98be3
Mukesh Potuganti <muksh.potuganti@gmail.com>
2019-05-21 20:34:20 -0700

    Staging changes are done

---
+++ uidesign/src/main/java/org/mcnies/uidesign/controller/PageController.java
@@ -0,0 +1,19 @@
+package org.mcnies.uidesign.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView("page");
+		
+		mv.addObject("greeting", "Welcome to spring mvc");
+		return mv;
+	}
+	
+}
